65dba36cc764aab000cd390b76905c8b91e5ca9f
Prashant Tankariya <leedonniee@gmail.com>
2021-07-20 15:54:04 +0530

    fix

--- push.js
+++ push.js
@@ -10,6 +10,6 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:leedonniee@gmail.com',vapidKeys.publicKey , vapidKeys.privateKey);
 
-let sub = {};
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/fZm9a9OcUM8:APA91bE0TbzvxrXFCOIS10BDZ5IwhUte9OyZsgtXDyQwARkFEW8Pwb7ZMiUlf3kzAuOF6gtXkEo0qFJUouN79Tdez6sQTCN0MttM-SZkEviQu6Uq79EkyqZVeaZI7htmXRT-4jMD3bkc","expirationTime":null,"keys":{"p256dh":"BCbkZYxigy4qp7GC8i72merCcUB0M_jrUkHqeKWKs2QgN0ZF0gPvxVLBHtch9XKpSP68kLO9Pl9EA7KzycEX-lo","auth":"JKsgUES9O6a4B56CxxiSvg"}};
 
 push.sendNotification(sub, 'test Message');
--- sw.js
+++ sw.js
@@ -1,5 +1,35 @@
-self.addEventListener('push', () => {
+self.addEventListener('push',function(e) {
 
-    self.registration.sendNotification('test Message',{});
+        var options = {
 
-})
+            body:'This notification is generated from the push.',
+            icon : 'images/example.png',
+            vibrate : [100,50,100],        
+            data: {
+
+                    dateOfArrival: Date.now(),
+                    primaryKey: '2'
+
+            },
+            actions: [
+
+                {
+                    action: 'explore', 
+                    title: 'Explore this new world.',
+                    icon : 'images/checkmark.png',
+                },
+                {
+                    action: 'close', 
+                    title: 'close',
+                    icon : 'images/xmark.png',
+                }
+                
+
+            ]
+        };
+        e.waitUntill(
+
+                self.registration.showNotification('Hello World',options)
+        );
+
+});
